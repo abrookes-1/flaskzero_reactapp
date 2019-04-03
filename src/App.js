2bdd7d9dbea2579cb6d110e1dce2878a380f509c
Aidan Brookes <aaabrookes@gmail.com>
2019-04-03 12:11:01 -0600

    added bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+//import logo from './logo.svg';
 import './App.css';
 import {
   Route,
@@ -13,19 +20,33 @@ import model from "./model";
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-      <div className="App">
-        <ul className="header">
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/model">Model</NavLink></li>
-        </ul>
-        <div className="content">
-          <Route exact path="/" component={home}/>
-          <Route exact path="/model" component={model}/>
-        </div>
+        <BrowserRouter>
 
-      </div>
-    </BrowserRouter>
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="/">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/model">SomeModel</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-info">Search</Button>
+            </Form>
+          </Navbar>
+
+          {/*<div className="App">*/}
+            {/*<ul className="header">*/}
+              {/*<li><NavLink to="/">Home</NavLink></li>*/}
+              {/*<li><NavLink to="/model">Model</NavLink></li>*/}
+            {/*</ul>*/}
+            <div className="content">
+              <Route exact path="/" component={home}/>
+              <Route exact path="/model" component={model}/>
+            </div>
+          {/*</div>*/}
+
+        </BrowserRouter>
     );
   }
 }
