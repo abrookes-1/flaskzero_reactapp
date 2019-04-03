3732b7196d57b8d8161ed52bc5810c28d3a90150
Aidan Brookes <aaabrookes@gmail.com>
2019-04-03 14:15:40 -0600

    changed functionality of navbar links

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import {
   FormControl,
   Button,
 } from "react-bootstrap";
+import {
+  LinkContainer
+} from "react-router-bootstrap"
 //import logo from './logo.svg';
 import './App.css';
 import {
@@ -25,9 +28,16 @@ class App extends Component {
           <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="/">Navbar</Navbar.Brand>
             <Nav className="mr-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/model">SomeModel</Nav.Link>
-              {/*<Nav.Link href="/pricing">Pricing</Nav.Link>*/}
+              <LinkContainer to="/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/model">
+                <Nav.Link>Model</Nav.Link>
+              </LinkContainer>
+              {/*<NavLink to="/">Home</NavLink>*/}
+              {/*<NavLink to="/model">Model</NavLink>*/}
+              {/*<Nav.Link href="/">Home</Nav.Link>*/}
+              {/*<Nav.Link href="/model">SomeModel</Nav.Link>*/}
             </Nav>
             <Form inline>
               <FormControl type="text" placeholder="Search" className="mr-sm-2" />
@@ -38,7 +48,7 @@ class App extends Component {
           {/*<div className="App">*/}
             {/*<ul className="header">*/}
               {/*<li><NavLink to="/">Home</NavLink></li>*/}
-              <li><NavLink to="/model">Model</NavLink></li>
+              {/*<li><NavLink to="/model">Model</NavLink></li>*/}
             {/*</ul>*/}
             <div className="content">
               <Route exact path="/" component={home}/>
